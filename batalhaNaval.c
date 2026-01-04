12e0a37305ef3c4be453f4d7944005dbdbbb8a88
Vinícius Soares <83523970+ViniciusSoares-Developer@users.noreply.github.com>
2026-01-04 20:13:31 +0000

    Criação do batalha naval, posicionamento dos navios e set de tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,101 @@
 #include <stdio.h>
-
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#include <stdbool.h>
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Variavel para guardar o navio
+    int navio[3] = {3, 3, 3};
+
+    /**
+     * Variavel para guardar o tabuleiro totalmente zerado
+     * 
+     * 0 -> Agua
+     * 3 -> Navio
+     */
+    int tabuleiro[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
+
+    int naviosColocados = 0; // Quantidade de navios já posicionados
+    int totalNavio = 2; // Total de navios há se posicionar
+
+    // Roda enquanto todos os navios não foram posicionados
+    while (naviosColocados < totalNavio) {
+        int coordenadaY, coordenadaX; // Variaveis para informar qual a primeira posição do barco
+        printf("Cordenadas iniciais do %d° navio (de 1 a 10): ", naviosColocados + 1);
+        // Scans para lê a entrada do usuário
+        scanf("%d", &coordenadaY);
+        scanf("%d", &coordenadaX);
+
+        // Retirar 1 utilizando pre decremento pos a entrada do usuário vem de 1 a 10
+        --coordenadaY;
+        --coordenadaX;
+        
+        /**
+         * Verificar se as coordenadas digitas está dentro dos limites do tabuleiro do jogo
+         * Caso não esteja dentro dos limites o jogo retorna a perguntar as coordenadas
+         * para coletar um novo posicionamento inicial para o navio
+         */
+        bool coordenadaYForaDoLimite = coordenadaY < 0 || coordenadaY > 9;
+        bool coordenadaXForaDoLimite = coordenadaX < 0 || coordenadaX > 9;
+        if (coordenadaYForaDoLimite || coordenadaXForaDoLimite) {
+            printf("Valor digitado para %s fora do limite do mapa.\n", coordenadaYForaDoLimite ? "Y" : "X");
+            continue; 
+        }
+        
+        // Loop aninhado para colocar os valores dentro do tabuleiro
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                /*
+                 * Para dinamismo todo barco par é horizontal e impar é vertical.
+                 * 
+                 * se as posições de Y e X batem com o da entrada ele seta os valores
+                 * caso já tenha navio posicionado ele retorna ao inicio e não posiciona o navio
+                 */
+                if (y == coordenadaY && x == coordenadaX) {
+                    if ((naviosColocados + 1) % 2 != 0 && coordenadaX <= 7) {
+                        if (tabuleiro[y][x] != 0 || tabuleiro[y][x + 1] != 0 || tabuleiro[y][x + 2] != 0) {
+                            printf("Não é possivel colocar o navio onde outro está!\n");
+                            break;
+                        }
+                        tabuleiro[y][x] = navio[0];
+                        tabuleiro[y][x + 1] = navio[1];
+                        tabuleiro[y][x + 2] = navio[2];
+                        ++naviosColocados;
+                    } else if (coordenadaY <= 7) {
+                        if (tabuleiro[y][x] != 0 || tabuleiro[y + 1][x] != 0 || tabuleiro[y + 2][x] != 0) {
+                            printf("Não é possivel colocar o navio onde outro está!\n");
+                            break;
+                        }
+                        tabuleiro[y][x] = navio[0];
+                        tabuleiro[y + 1][x] = navio[1];
+                        tabuleiro[y + 2][x] = navio[2];
+                        ++naviosColocados;
+                    } else {
+                        printf("Navio parte fora do limite!\n");
+                        break;
+                    }
+                }
+            }
+        }
+        
+    }
+
+    // Exibi o tabuleiro
+    for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < 10; x++) {
+            printf("%d ", tabuleiro[y][x]);
+        }
+        printf("\n");
+    }
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
